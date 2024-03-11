2c1f08ac3b4d11e0913b13b24109b272673e52a1
unknown <zheng.hui94@gmail.com>
2024-03-11 11:36:07 +0800

    added Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Form = () => {
   const [inputState, setInputState] = useState({
@@ -11,10 +13,61 @@ const Form = () => {
   });
 
   const { title, amount, date, category, description } = inputState;
+  const handleInput = (name) => (e) => {
+    setInputState({ ...inputState, [name]: e.target.value });
+  };
+
   return (
     <FormStyled>
       <div className="input-control">
-        <input type="text" />
+        <input
+          type="text"
+          value={title}
+          name={"title"}
+          placeholder="Salary Title"
+          onChange={handInput("title")}
+        />
+      </div>
+      <div className="input-control">
+        <input
+          type="text"
+          value={amount}
+          name={"amount"}
+          placeholder="Salary Amount"
+          onChange={handInput("amount")}
+        />
+      </div>
+      <div className="input-control">
+        <DatePicker
+          id="date"
+          placeholderText="Enter A Date"
+          selected={date}
+          dateFormat="dd/MM/yyyy"
+          onChange={(date) => {
+            setInputState({ ...inputState, date: date });
+          }}
+        />
+      </div>
+      <div className="selects input-control">
+        <select
+          required
+          value={category}
+          name="category"
+          id="category"
+          onChange={handleInput("category")}
+        >
+          <option value="" disabled>
+            Select Option
+          </option>
+          <option value="salary">Salary</option>
+          <option value="freelancing">Freelancing</option>
+          <option value="investments">Investiments</option>
+          <option value="stocks">Stocks</option>
+          <option value="bitcoin">Bitcoin</option>
+          <option value="bank">Bank Transfer</option>
+          <option value="youtube">Youtube</option>
+          <option value="other">Other</option>
+        </select>
       </div>
     </FormStyled>
   );
